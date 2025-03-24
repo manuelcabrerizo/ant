24f2ab083b0998405218fa0c430cf2bac88398a1
manuel <manuelcabrerizo5@gmail.com>
2025-03-23 21:17:52 -0300

    clean the assimp lib folder

--- build.bat
+++ build.bat
@@ -4,11 +4,11 @@ if not exist .\build mkdir .\build
 
 set TARGET=ant
 set CFLAGS=/W2 /nologo /Od /Zi /std:c++14
-set LIBS=User32.lib dxguid.lib D3D11.lib D3DCompiler.lib assimp-vc143-mtd.lib
+set LIBS=User32.lib dxguid.lib D3D11.lib D3DCompiler.lib assimp-vc143-mt.lib
 set SOURCES=.\src\win32_platform.cpp
 set OUT_DIR=/Fo.\build\ /Fe.\build\%TARGET% /Fm.\build\
 set INC_DIR=/Ilibs\glm /Ilibs\stb /Ilibs\assimp\include
-set LNK_DIR=/LIBPATH:libs\assimp\lib\Debug
+set LNK_DIR=/LIBPATH:libs\assimp\lib\Release
 set DEFINES=/DANT_DEBUG /DANT_CONSOLE
 
 echo on
