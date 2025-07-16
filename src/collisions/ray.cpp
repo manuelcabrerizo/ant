221d34394e03b786039cbaed73d5dcfc7ba59e46
Manuel Cabrerizo <manuelcabrerizo5@gmail.com>
2025-07-16 15:41:46 -0300

    obb create invRotMat directly

--- src/collisions/ray.cpp
+++ src/collisions/ray.cpp
@@ -260,12 +260,10 @@ bool Ray::Intersect(const OBB& obb, float& t) const
     Vector3 z = obb.GetOrientation(2);
 
     Matrix4 invRotMat = Matrix4(
-        x[0], y[0], z[0], 0,
-        x[1], y[1], z[1], 0,
-        x[2], y[2], z[2], 0,
+        x[0], x[1], x[2], 0,
+        y[0], y[1], y[2], 0,
+        z[0], z[1], z[2], 0,
         0, 0, 0, 1);
-
-    invRotMat = Matrix4::Transposed(invRotMat);
 
     Vector3 relOrigin = Matrix4::TransformPoint(invRotMat, o - obb.GetCenter());
     Vector3 relDirection = Matrix4::TransformVector(invRotMat, d).Normalized();
